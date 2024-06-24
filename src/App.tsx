2fc341a923da78895c6231ec6c8d273fdef2f1c5
Chrizzoman <sasuke123451997@gmail.com>
2024-06-24 12:23:37 +0300

    Push

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components/layout/layout'
+import { Router } from '@/router'
+
+export function App() {
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  )
+}
---
+++ src/pages/decks.page.tsx
@@ -0,0 +1,5 @@
+import { Page } from '@/components/ui/page/page'
+
+export function DecksPage() {
+  return <Page>Decks</Page>
+}
